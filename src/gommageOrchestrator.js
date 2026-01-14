bd9e616f4a6090f62237b68144996cccebcfd7f8
Thib <introvigne.t@gmail.com>
2026-01-14 18:48:01 +0100

    Dissolve the text

--- src/gommageOrchestrator.js
+++ src/gommageOrchestrator.js
@@ -2,13 +2,43 @@
 
 import * as THREE from "three/webgpu";
 import MSDFText from "./msdfText.js";
+import { uniform } from "three/tsl";
+import Debug, { DEBUG_FOLDERS } from "./debug.js";
+import gsap from "gsap";
+
 export default class GommageOrchestrator {
+    #uProgress = uniform(0.0);
     constructor() {
     }
 
     async initialize(scene) {
+        const debugFolder = Debug.getInstance().getFolder(DEBUG_FOLDERS.MSDF_TEXT);
         const MSDFTextEntity = new MSDFText();
-        const msdfText = await MSDFTextEntity.initialize("WebGPU Gommage Effect", new THREE.Vector3(0, 0, 0));
+        const msdfText = await MSDFTextEntity.initialize("WebGPU Gommage Effect", new THREE.Vector3(0, 0, 0), this.#uProgress);
         scene.add(msdfText);
+        const GommageButton = debugFolder.addButton({
+            title: "GOMMAGE",
+        });
+        const ResetButton = debugFolder.addButton({
+            title: "RESET",
+        });
+        GommageButton.on("click", () => {
+            this.triggerGommage();
+        });
+        ResetButton.on("click", () => {
+            this.resetGommage();
+        });
+    }
+
+    triggerGommage() {
+        gsap.to(this.#uProgress, {
+            value: 1,
+            duration: 4,
+            ease: "linear",
+        });
+    }
+
+    resetGommage() {
+       this.#uProgress.value = 0;
     }
 }
--- src/msdfText.js
+++ src/msdfText.js
@@ -2,12 +2,14 @@
 
 import * as THREE from "three/webgpu";
 import { MSDFTextGeometry, MSDFTextNodeMaterial } from "three-msdf-text-utils";
+import { texture, mix, uniform, clamp, pow, attribute, step, float, smoothstep } from "three/tsl";
+import Debug, { DEBUG_FOLDERS } from "./debug.js";
 
 export default class MSDFText {
     constructor() {
     }
 
-    async initialize(text = "WebGPU Gommage Effect", position = new THREE.Vector3(0, 0, 0)) {
+    async initialize(text = "WebGPU Gommage Effect", position = new THREE.Vector3(0, 0, 0), uProgress) {
         // Load font data
         const response = await fetch("/fonts/Cinzel/Cinzel.json");
         const fontData = await response.json();
@@ -22,6 +24,14 @@ export default class MSDFText {
         fontAtlasTexture.wrapT = THREE.ClampToEdgeWrapping;
         fontAtlasTexture.generateMipmaps = false;
 
+        const perlinTexture = await textureLoader.loadAsync("/textures/perlin.webp");
+        perlinTexture.colorSpace = THREE.NoColorSpace;
+        perlinTexture.minFilter = THREE.LinearFilter;
+        perlinTexture.magFilter = THREE.LinearFilter;
+        perlinTexture.wrapS = THREE.RepeatWrapping;
+        perlinTexture.wrapT = THREE.RepeatWrapping;
+        perlinTexture.generateMipmaps = false;
+
         // Create text geometry
         const textGeometry = new MSDFTextGeometry({
             text,
@@ -29,10 +39,9 @@ export default class MSDFText {
             width: 1000,
             align: "center",
         });
+        console.log(textGeometry.attributes);
 
-        const textMaterial = new MSDFTextNodeMaterial({
-            map: fontAtlasTexture,
-        });
+        const textMaterial = this.createTextMaterial(fontAtlasTexture, perlinTexture, uProgress);
 
         // Adjust to remove visual artifacts
         textMaterial.alphaTest = 0.1;
@@ -49,5 +58,42 @@ export default class MSDFText {
         mesh.rotation.x = Math.PI;
         return mesh;
 
+    }
+
+
+    createTextMaterial(fontAtlasTexture, perlinTexture, uProgress) {
+        const textMaterial = new MSDFTextNodeMaterial({
+            map: fontAtlasTexture,
+            transparent: true,
+        });
+
+        const glyphUv = attribute("glyphUv", "vec2");
+        const center = attribute("center", "vec2");
+
+        const uNoiseRemapMin = uniform(0.4);
+        const uNoiseRemapMax = uniform(0.87);
+        const uCenterScale = uniform(0.05);
+        const uGlyphScale = uniform(0.75);
+        const uDissolvedColor = uniform(new THREE.Color("#5E5E5E"));
+        const uDesatComplete = uniform(0.45);
+        const uBaseColor = uniform(new THREE.Color("#ECCFA3"));
+
+        const customUv = center.mul(uCenterScale).add(glyphUv.mul(uGlyphScale));
+
+        const perlinTextureNode = texture(perlinTexture, customUv).x;
+        const perlinRemap = clamp(
+            perlinTextureNode.sub(uNoiseRemapMin).div(uNoiseRemapMax.sub(uNoiseRemapMin)),
+            0,
+            1
+        );
+        const dissolve = step(uProgress, perlinRemap);
+        const desaturationProgress = smoothstep(float(0.0), uDesatComplete, uProgress);
+
+        const colorMix = mix(uBaseColor, uDissolvedColor, desaturationProgress);
+        textMaterial.colorNode = colorMix;
+        const msdfOpacity = textMaterial.opacityNode;
+        textMaterial.opacityNode = msdfOpacity.mul(dissolve);
+
+        return textMaterial;
     }
 }
